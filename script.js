3c5773ada19ae2749025c53fe1f5cd08db74d0b6
Dylan Bryckaert <dylan.bryckaert@gmail.com>
2025-01-09 08:43:50 +0100

    personnages sélectionnés sur la page de combat

--- script.js
+++ script.js
@@ -1,77 +1,154 @@
 const launchGameButton = document.querySelector(".launch-game-button");
 const startingPage = document.querySelector(".starting-page");
 const selectChamp = document.querySelector(".select-champ");
+const fightingPage = document.querySelector(".fighting-page");
 const charactersContainer = document.querySelector(".character-container");
 
 let currentPlayer = 1;
 
-const characters = [
+const charactersList = [
   {
     name: "HTML",
     splashart: "./assets/splasharts/html.png",
     cover: "./assets/covers/html.png",
+    maxHealth: 1700,
+    health: 1700,
+    attack: 100,
+    defence: 100,
+    maxPower: 100,
+    power: 0,
+    charge: 5,
   },
   {
     name: "CSS",
     splashart: "./assets/splasharts/css.png",
     cover: "./assets/covers/css.png",
+    maxHealth: 1700,
+    health: 1700,
+    attack: 100,
+    defence: 100,
+    maxPower: 100,
+    power: 0,
+    charge: 5,
   },
   {
     name: "JavaScript",
     splashart: "./assets/splasharts/javascript.png",
     cover: "./assets/covers/javascript.png",
+    maxHealth: 1700,
+    health: 1700,
+    attack: 100,
+    defence: 100,
+    maxPower: 100,
+    power: 0,
+    charge: 5,
   },
   {
     name: "PhP",
     splashart: "./assets/splasharts/php.png",
     cover: "./assets/covers/php.png",
+    maxHealth: 1700,
+    health: 1700,
+    attack: 100,
+    defence: 100,
+    maxPower: 100,
+    power: 0,
+    charge: 5,
   },
   {
     name: "Python",
     splashart: "./assets/splasharts/python.png",
     cover: "./assets/covers/python.png",
+    maxHealth: 1700,
+    health: 1700,
+    attack: 100,
+    defence: 100,
+    maxPower: 100,
+    power: 0,
+    charge: 5,
   },
   {
     name: "SQL",
     splashart: "./assets/splasharts/sql.png",
     cover: "./assets/covers/sql.png",
+    maxHealth: 1700,
+    health: 1700,
+    attack: 100,
+    defence: 100,
+    maxPower: 100,
+    power: 0,
+    charge: 5,
   },
   {
     name: "React",
     splashart: "./assets/splasharts/react.png",
     cover: "./assets/covers/react.png",
+    maxHealth: 1700,
+    health: 1700,
+    attack: 100,
+    defence: 100,
+    maxPower: 100,
+    power: 0,
+    charge: 5,
   },
   {
     name: "Java",
     splashart: "./assets/splasharts/java.png",
     cover: "./assets/covers/java.png",
+    maxHealth: 1700,
+    health: 1700,
+    attack: 100,
+    defence: 100,
+    maxPower: 100,
+    power: 0,
+    charge: 5,
   },
 ];
 
 /**
  * @summary Disparition en fondu de la starting page et apparition en fondu de la prochaine page
  */
-const toggleView = () => {
-  startingPage.classList.add("fade-out");
+const toggleView = (page) => {
+  if (page.classList.contains("starting-page")) {
+    startingPage.classList.add("fade-out");
 
-  startingPage.addEventListener(
-    "animationend",
-    () => {
-      startingPage.style.display = "none";
-      startingPage.classList.remove("fade-out");
+    startingPage.addEventListener(
+      "animationend",
+      () => {
+        startingPage.style.display = "none";
+        startingPage.classList.remove("fade-out");
 
-      selectChamp.style.display = "flex";
-      selectChamp.classList.add("fade-in");
+        selectChamp.style.display = "flex";
+        selectChamp.classList.add("fade-in");
 
-      setTimeout(() => {
-        selectChamp.classList.add("visible");
-      }, 50);
-    },
-    { once: true },
-  );
+        setTimeout(() => {
+          selectChamp.classList.add("visible");
+        }, 50);
+      },
+      { once: true },
+    );
+  } else {
+    selectChamp.classList.add("fade-out");
+
+    selectChamp.addEventListener(
+      "animationend",
+      () => {
+        selectChamp.style.display = "none";
+        selectChamp.classList.remove("fade-out");
+
+        fightingPage.style.display = "flex";
+        fightingPage.classList.add("fade-in");
+
+        setTimeout(() => {
+          fightingPage.classList.add("visible");
+        }, 50);
+      },
+      { once: true },
+    );
+  }
 };
 
-launchGameButton.addEventListener("click", () => toggleView());
+launchGameButton.addEventListener("click", () => toggleView(startingPage));
 
 /**
  * @summary Retire la classe "force" qui force les images P1 et P2 à rester affichées
@@ -94,15 +171,15 @@ const handleHover = (img, display) => {
   img.style.display = display ? "block" : "none";
 };
 
+let player1Character;
+let player2Character;
+
 /**
  * @summary Affiche le splashart du personnage
  * @param {*} character personnage
  * @param {*} cover source de l'image
- * @returns
  */
-
 const showSplashart = (player, character, cover) => {
-  if (player === 3) return;
   const splashartLeft = document.querySelector(".splashart-left");
   const splashartRight = document.querySelector(".splashart-right");
 
@@ -113,19 +190,24 @@ const showSplashart = (player, character, cover) => {
   );
 
   if (player === 1) {
-    splashartLeft.innerHTML = ""; /* on vide le dom */
+    splashartLeft.innerHTML = "";
     const splashart = document.createElement("img");
     splashart.src = character.splashart;
     splashart.classList.add("splashart");
     splashartLeft.appendChild(splashart);
     cover.classList.add("active");
+    player1Character = character;
   } else {
-    splashartRight.innerHTML = ""; /* on vide le dom */
+    splashartRight.innerHTML = "";
     const splashart = document.createElement("img");
     splashart.src = character.splashart;
     splashart.classList.add("splashart");
     splashartRight.appendChild(splashart);
     cover.classList.add("active");
+    player2Character = character;
+    setTimeout(() => {
+      launchGame(player1Character, player2Character);
+    }, 1000);
   }
 };
 
@@ -133,7 +215,7 @@ const showSplashart = (player, character, cover) => {
  * @summary Génère la liste des personnages et leurs eventListener
  */
 const generateCovers = () => {
-  characters.forEach((character) => {
+  charactersList.forEach((character) => {
     const coverContainer = document.createElement("div");
     coverContainer.classList.add("cover-container");
 
@@ -173,8 +255,6 @@ const generateCovers = () => {
 
 generateCovers();
 
-const annuler = document.querySelector(".annuler");
-annuler.addEventListener("click", retour);
 function retour() {
   const splashartLeft = document.querySelector(".splashart-left");
   const splashartRight = document.querySelector(".splashart-right");
@@ -196,16 +276,151 @@ function retour() {
     });
   }
 
+  const annuler = document.querySelector(".annuler");
+  annuler.addEventListener("click", retour);
+
   // Réduire currentPlayer tout en s'assurant qu'il ne descend pas en dessous de 1
   currentPlayer = Math.max(currentPlayer - 1, 1);
-  console.log(currentPlayer);
 }
 
-/*
-  après avoir sélectionné les deux persos:
-  prend en paramètre les classes
-  select champ dégage, page de combat s'affiche
-  J1 à gauche et J2 à droite
-  Bouton attaque, défense, super (grisé tant que power par chargé)
-  Charge en-dessous de chaque perso
-*/
+class Character {
+  constructor(name, health, attack, defence, power, charge) {
+    this.name = name;
+    this.health = health;
+    this.attack = attack;
+    this.defence = defence;
+    this.power = power;
+    this.charge = charge;
+  }
+
+  launchAttack(target) {}
+
+  protect(attacker) {}
+}
+
+const generateHealthAndPowerBars = (p1Char, p2Char) => {
+  const frameP1 = document.querySelector(".frame-player-one");
+  const frameP2 = document.querySelector(".frame-player-two");
+
+  for (let i = 1; i <= 2; i++) {
+    const avatar = document.createElement("img");
+    avatar.src = i === 1 ? p1Char.cover : p2Char.cover;
+    i === 1 ? frameP1.appendChild(avatar) : frameP2.appendChild(avatar);
+
+    const healthBar = document.createElement("div");
+    healthBar.classList.add(`health-bar-p${i}`);
+
+    const hp = document.createElement("div");
+    hp.classList.add(`hp-p${i}`);
+    healthBar.appendChild(hp);
+
+    const lostHp = document.createElement("div");
+    lostHp.classList.add(`lost-hp-p${i}`);
+    healthBar.appendChild(lostHp);
+
+    i === 1 ? frameP1.appendChild(healthBar) : frameP2.appendChild(healthBar);
+
+    const powerBar = document.createElement("div");
+    powerBar.classList.add(`power-bar-p${i}`);
+
+    const power = document.createElement("div");
+    power.classList.add(`power-p${i}`);
+    powerBar.appendChild(power);
+
+    const powerRemaiming = document.createElement("div");
+    powerRemaiming.classList.add(`power-remaiming-p${i}`);
+    powerBar.appendChild(powerRemaiming);
+
+    i === 1 ? frameP1.appendChild(powerBar) : frameP2.appendChild(powerBar);
+  }
+};
+
+let player1Champion;
+let player2Champion;
+
+const launchGame = (p1Char, p2Char) => {
+  currentPlayer = 1;
+  toggleView(selectChamp);
+  generateHealthAndPowerBars(p1Char, p2Char);
+
+  player1Champion = new Character(
+    player1Character.name,
+    player1Character.health,
+    player1Character.attack,
+    player1Character.defence,
+    player1Character.power,
+    player1Character.charge,
+  );
+  player2Champion = new Character(
+    player2Character.name,
+    player2Character.health,
+    player2Character.attack,
+    player2Character.defence,
+    player2Character.power,
+    player2Character.charge,
+  );
+};
+
+let player1Choice;
+let player2Choice;
+
+const getChoice = (choice) => {
+  currentPlayer === 1 ? (player1Choice = choice) : (player2Choice = choice);
+  return choice;
+};
+
+const playAction = (action) => {
+  console.log(player1Choice, player2Choice);
+  switch (action) {
+    case "attack-button":
+      if (currentPlayer === 1) {
+        player1Champion.launchAttack(player2Champion);
+        currentPlayer = 2;
+      } else {
+        player2Champion.launchAttack(player1Champion);
+        if (checkWin(player1Champion, player2Champion)) {
+          return endGame();
+        }
+
+        currentPlayer = 1;
+      }
+      break;
+
+    case "defence-button":
+      if (currentPlayer === 1) {
+        player1Champion.protect(player2Champion);
+        currentPlayer = 2;
+      } else {
+        player2Champion.protect(player1Champion);
+        currentPlayer = 1;
+      }
+      break;
+
+    case "power-button":
+      console.log("Super pouvoir!");
+      break;
+
+    default:
+      break;
+  }
+};
+
+const choicesButtons = document.querySelector(".action-buttons").children;
+for (let i = 0; i < choicesButtons.length; i++) {
+  const choice = choicesButtons[i].classList[0];
+  choicesButtons[i].addEventListener("click", () =>
+    playAction(getChoice(choice)),
+  );
+}
+
+const checkWin = (target1, target2) => {
+  if (target1.health === 0 && target2.health === 0) {
+    console.log("Égalité!");
+    return true;
+  } else if (target1.health === 0 || target2.health === 0) return true;
+  else return false;
+};
+
+const endGame = () => {
+  console.log("Jeu terminé");
+};
